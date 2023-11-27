1c49b97e332624d5fb801a586acfff34b0c2d62c
Md Shawon Reza <fahadshawon241@gmail.com>
2023-11-27 22:30:16 +0600

    add this Delete method for delate a meal data

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
             res.send(result)
           })
 
+        app.delete('/meals/:id',async(req,res)=>{
+            const id=req.params.id
+            const query={_id: new ObjectId(id)}
+            const result=await mealCollection.deleteOne(query)
+            res.send(result)
+        })  
+
 
 
         //   user related api
